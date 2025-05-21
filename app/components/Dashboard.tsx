1826951d9fa7ac4b33741657341e857dead5eb65
Kelvin Olaiya <kelvin.olaiya@unibo.it>
2025-05-21 17:42:07 +0200

    chore: add alert message

--- app/components/Dashboard.tsx
+++ app/components/Dashboard.tsx
@@ -4,14 +4,13 @@ import mqtt from "mqtt";
 type Pedestrian = {
   x: number;
   y: number;
+  distance: number;
 }
 
 function Dashboard() {
   const canvasRef = useRef<HTMLCanvasElement>(null);
   const client = useRef(mqtt.connect("ws://localhost:8080"));
   const [pedestrians, setPedestrians] = useState<Pedestrian[]>([]);
-
-  
 
   function clearCanvas(ctx: CanvasRenderingContext2D) {
     ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height);
@@ -29,11 +28,26 @@ function Dashboard() {
     };
   }
 
+  function drawPedestrian(ctx: CanvasRenderingContext2D, pedestrian: Pedestrian) {
+    const pedestrianImage = new Image();
+    pedestrianImage.src = "/pedestrian.png";
+    pedestrianImage.onload = () => {
+      ctx.beginPath();
+      ctx.fillStyle = "rgba(150, 255, 0, 0.9)";
+      ctx.arc(pedestrian.x + 25, pedestrian.y + 40, 50, 0, Math.PI * 2);
+      ctx.fill();
+      ctx.drawImage(pedestrianImage, pedestrian.x, pedestrian.y, 50, 50);
+      ctx.font = "20px Arial";
+      ctx.fillStyle = "black";
+      ctx.fillText(`${pedestrian.distance} m`, pedestrian.x-10, pedestrian.y + 70);
+    };
+  }
+
   function drawLaneLines(ctx: CanvasRenderingContext2D) {
     ctx.strokeStyle = "white";
     ctx.lineWidth = 10;
-    ctx.setLineDash([]);
     ctx.beginPath();
+    ctx.setLineDash([]);
     ctx.moveTo(150, 600);
     ctx.lineTo(200, 0);
     ctx.stroke();
@@ -73,22 +87,22 @@ function Dashboard() {
     if (ctx) {
       clearCanvas(ctx);
       pedestrians.forEach((pedestrian) => {
-        ctx.fillStyle = "red";
-        ctx.beginPath();
-        ctx.arc(pedestrian.x, pedestrian.y, 5, 0, Math.PI * 2);
-        ctx.fillText(`(${pedestrian.x}, ${pedestrian.y})`, pedestrian.x + 10, pedestrian.y);
-        ctx.fill();
-        ctx.fillStyle = "black";
-        ctx.lineTo(100, 100);
-        ctx.stroke();
+        drawPedestrian(ctx, pedestrian);
       });
     }
   }, [pedestrians]);
 
   return (
     <div>
-      <canvas ref={canvasRef} id="canvas" width="800" height="600" className="border mb-6 rounded-lg">
-      </canvas>
+     <canvas ref={canvasRef} id="canvas" width="1200" height="600" className="border mb-6 rounded-lg"></canvas>
+      <div className="text-center">
+        <div className="bg-yellow-400 rounded-full text-center mb-4 p-10">
+          <h1 className="text-4xl font-bold">SLOW DOWN!</h1>
+          <p className="text-lg">Pedestrian detected ahead</p>
+          <p className="text-9xl font-bold">{pedestrians[0]?.distance} m</p>
+          <p className="text-4xl">Estimating <strong>{pedestrians[0]?.distance / 2}s</strong> to collision</p>
+        </div>
+      </div>
       <h1 className="text-2xl font-bold mt-4">Dashboard</h1>
       <ul>
         {pedestrians.map((pedestrian, index) => (
